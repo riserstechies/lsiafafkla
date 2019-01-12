34a3bceb98a419096c3dfb063d716611510c627e
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-12 22:57:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.amazon.com/Sistema-Collection-Dressing-Container-Purple/dp/B00BG4DH90/ref=lp_18505439011_1_7?srs=18505439011&ie=UTF8&qid=1547313983&sr=8-7">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.amazon.com/Sistema-Collection-Dressing-Container-Purple/dp/B00BG4DH90/ref=lp_18505439011_1_7?srs=18505439011&ie=UTF8&qid=1547313983&sr=8-7"
         </script>
         <title>Page Redirection</title>
     </head>
